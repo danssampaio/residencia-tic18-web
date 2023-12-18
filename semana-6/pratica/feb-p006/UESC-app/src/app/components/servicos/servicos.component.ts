1becebdf138322c01f0fd549a96e5047011c1193
Dan Souza <darleyble3@gmail.com>
2023-12-17 22:26:13 -0300

    update folders structures and add api data

---
+++ semana-6/pratica/feb-p006/UESC-app/src/app/components/servicos/servicos.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ServicosService } from '../../services/servicos.service';
+
+@Component({
+  selector: 'app-servicos',
+  templateUrl: './servicos.component.html',
+  styleUrls: ['./servicos.component.css'],
+})
+
+export class ServicosComponent implements OnInit {
+  servicos: any[] = [];
+
+  constructor(private servicosService: ServicosService) {}
+
+  ngOnInit(): void {
+    this.servicosService.getServicos().subscribe(
+      (data) => {
+        this.servicos = data;
+      },
+      (error) => {
+        console.error('Erro ao obter serviços:', error);
+      }
+    );
+  }
+}
