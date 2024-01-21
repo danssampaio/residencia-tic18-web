adaf5fc8bd8303f8fb60eecbf01ef8b21e5dac52
Dan Souza <darleyble3@gmail.com>
2024-01-21 20:21:12 -0300

    update

--- JReader/src/app/directives/carrinho-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CarrinhoHighlightDirective } from './carrinho-highlight.directive';
-
-describe('CarrinhoHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CarrinhoHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- JReader/src/app/directives/class-highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ClassHighlightDirective } from './class-highlight.directive';
-
-describe('ClassHighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClassHighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
