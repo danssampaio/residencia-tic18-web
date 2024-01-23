2960d228be08bfd2690ef569d1850b7582a4edfb
ticihoog <ticianapaternostro@gmail.com>
2024-01-23 11:18:01 -0300

    criacao resultado-pesquisa

---
+++ semana-8/pratica/feb-p008/WikiApi/src/app/components/resultado-pesquisa/resultado-pesquisa.component.ts
@@ -0,0 +1,12 @@
+// resultado-pesquisa.component.ts
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-resultado-pesquisa',
+  templateUrl: './resultado-pesquisa.component.html',
+  styleUrls: ['./resultado-pesquisa.component.css']
+})
+export class ResultadoPesquisaComponent {
+  @Input() resultados: any[] = [];
+  @Input() termoPesquisa: string = "";
+}
