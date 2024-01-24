1646eb955d0b9bbf03edfa25b055ad4e7173bf2b
Dan Souza <darleyble3@gmail.com>
2024-01-24 09:58:35 -0300

    navbar create

--- semana-8/pratica/feb-p008/WikiApi/src/app/app.module.ts
+++ semana-8/pratica/feb-p008/WikiApi/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BarraPesquisaComponent } from './components/barra-pesquisa/barra-pesqui
 import { BoldSearchPipe } from './pipes/bold-search.pipe';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { HttpClientModule } from '@angular/common/http';
     ResultadoPesquisaComponent,
     BarraPesquisaComponent,
     BoldSearchPipe,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
