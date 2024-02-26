ee7bc537ed594d7bb6e9483c7a6b9e986a5342ca
Dan Souza <darleyble3@gmail.com>
2024-02-26 01:15:29 -0300

    po11 add

---
+++ semana-11/pratica/feb-p011/Form/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsComponent } from './components/forms/forms.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
